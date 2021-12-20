9d139cc2aa92ed8373a22ce445039f5eed2b009e
Sculptex <sculptex@users.noreply.github.com>
2021-12-20 22:15:53 +0000

    Update secrun.go

--- secrun.go
+++ secrun.go
@@ -9,7 +9,7 @@ import (
 	"time"
 	"flag"
 	"os/exec"
-
+	"os"
 )
 
 const VERSION = "0.0.1"
@@ -17,22 +17,28 @@ const DEFAULTFREQ = 1
 
 func main() {
 
-    var cmd string    
-    var version bool    
+    var cmd string
+    var version bool
     var freq int64
     var max int64
-    	
+
     flag.BoolVar(&version, "version", false, "Show Version info")
     flag.StringVar(&cmd, "cmd", cmd, "Bash Command To Run")
     flag.Int64Var(&freq, "freq", DEFAULTFREQ, "Frequency in Seconds")
     flag.Int64Var(&max, "max", 0, "Max Duration in Seconds, (0 infinite)")
-    
-    flag.Parse()  
-    
+
+
+    flag.Parse()
+
+	if(version) {
+		fmt.Printf("VERSION %s\n", VERSION)
+		os.Exit(0)
+	}
+
 	go crun(cmd, freq)
-	
+
 	maxsecs , _:= time.ParseDuration(fmt.Sprintf("%ds", max))
-	
+
 	if(maxsecs > 0) {
 		time.Sleep( maxsecs )
 	} else {
@@ -45,13 +51,13 @@ func main() {
 func crun(cmd string, freq int64) {
 	var cmdarray []string
 	var o []byte
-	
+
 	cmdarray = []string{
 			"bash",
 			cmd}
 	head := cmdarray[0]
-	parts := cmdarray[1:len(cmdarray)]			
-	
+	parts := cmdarray[1:len(cmdarray)]
+
 	freqsecs , _ := time.ParseDuration(fmt.Sprintf("%ds", freq))
 	for range time.Tick( freqsecs ) {
 		o , _ = exec.Command(head,parts...).Output()
